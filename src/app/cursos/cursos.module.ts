bb500b1d7eb41230c77276df168142a847ae17fc
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-05-31 22:20:16 -0300

    Melhorando o css da tabela, criando toolbar e refatorando os imports

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,9 +1,10 @@
+import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
 import { CursosRoutingModule } from './cursos-routing.module';
 import { CursosComponent } from './cursos/cursos.component';
+
 
 
 
@@ -14,7 +15,7 @@ import { CursosComponent } from './cursos/cursos.component';
   imports: [
     CommonModule,
     CursosRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CursosModule { }
